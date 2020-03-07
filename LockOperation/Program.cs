21e1d13f7124de3a81edeb929a7992bf02608e33
Filippo <f.izum02@gmail.com>
2020-03-07 12:12:42 +0100

    Aggiungere i file di progetto.

---
+++ LockOperation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(ThreadUnsafe.Go);
+            Task.Factory.StartNew(ThreadUnsafe.Go);
+            Task.Factory.StartNew(ThreadUnsafe.Go);
+            Task.Factory.StartNew(ThreadUnsafe.Go);
+            Task.Factory.StartNew(ThreadUnsafe.Go);
+
+            Console.ReadLine();
+        }
+    }
+
+    class ThreadUnsafe
+    {
+        public static object myObj = new object();
+        static int _val1 = 1, _val2 = 1;
+
+        public static void Go()
+        {
+            //lock (myObj)
+            Monitor.Enter(myObj);
+            try
+            {
+                if (_val2 != 0)
+                {
+                    Console.WriteLine(_val1 / _val2);
+                    _val2 = 0;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Operazione non eseguibile");
+            }
+            finally
+            {
+                Monitor.Exit(myObj);
+            }
+        }
+    }
+}
